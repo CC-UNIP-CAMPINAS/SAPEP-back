24de158029a25bda8f7a1df1db323da927e3baa6
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-09-14 19:50:03 -0300

    refactor: :recycle: troca do sequelize pelo prisma

--- src/api/controllers/GroupController.js
+++ src/api/controllers/GroupController.js
@@ -1,14 +1,18 @@
-const database = require("../services/sequelize/models");
+const PrismaService = require("../services/prisma/prisma.service");
 
 class GroupController {
-  static async getAll(_, res) {
-    try {
-      const allGroups = await database.Group.findAll();
-      return res.status(200).json(allGroups);
-    } catch (error) {
-      return res.status(500).json(error.message);
+    constructor() {
+        this.groups = new PrismaService().groups;
     }
-  }
+
+    async findAll(_, res) {
+        try {
+            const allGroups = await this.groups.findMany({});
+            return res.status(200).json(allGroups);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = GroupController;
