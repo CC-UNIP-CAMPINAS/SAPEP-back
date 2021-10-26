5fddeccdcefc89410093bd68900dd48d50c90f06
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-10-25 22:55:48 -0300

    refactor(Patient): :recycle: Adicionado os grupos de permissão corretos

--- src/api/routes/Patient.routes.js
+++ src/api/routes/Patient.routes.js
@@ -5,35 +5,27 @@ const patientController = new PatientController();
 const { verifyJWT, verifyGroup } = require("../middlewares/auth.middleware");
 
 module.exports = (app) => {
-  app.get(
-    "/patient/:id",
-    [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT"])],
-    (req, res) => {
-      patientController.findOneById(req, res);
-    }
-  );
+    app.get(
+        "/patient/:id",
+        [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT", "MEDICOS", "ENFERMAGEM", "ADM"])],
+        (req, res) => {
+            patientController.findOneById(req, res);
+        }
+    );
 
-  app.get(
-    "/patient",
-    [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT"])],
-    (req, res) => {
-      patientController.findAll(req, res);
-    }
-  );
+    app.get(
+        "/patient",
+        [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT", "MEDICOS", "ENFERMAGEM", "ADM"])],
+        (req, res) => {
+            patientController.findAll(req, res);
+        }
+    );
 
-  app.post(
-    "/patient",
-    [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT"])],
-    (req, res) => {
-      patientController.create(req, res);
-    }
-  );
+    app.post("/patient", [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT", "ADM"])], (req, res) => {
+        patientController.create(req, res);
+    });
 
-  app.delete(
-    "/patient/:id",
-    [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT"])],
-    (req, res) => {
-      patientController.delete(req, res);
-    }
-  );
+    app.delete("/patient/:id", [verifyJWT, (req, res, next) => verifyGroup(req, res, next, ["ROOT"])], (req, res) => {
+        patientController.delete(req, res);
+    });
 };
