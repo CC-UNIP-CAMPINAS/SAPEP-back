3e2c920f1e3a1db91bc28e435a50cf47a54e4980
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-11-12 00:19:53 -0300

    feat: :sparkles: Aplicando express validation nas rotas de linkReset

--- src/api/routes/LinkResetEmail.routes.js
+++ src/api/routes/LinkResetEmail.routes.js
@@ -1,13 +1,26 @@
 const LinkResetEmailController = require("../controllers/LinkResetEmailController");
 const { verifyJWT } = require("../middlewares/auth.middleware");
+const { validate, Joi } = require("express-validation");
 
 module.exports = (app) => {
+    const createValidation = {
+        body: Joi.object({
+            newEmail: Joi.string().required(),
+        }),
+    };
+
+    const confirmValidation = {
+        params: Joi.object({
+            id: Joi.string().required(),
+        }),
+    };
+
     const controller = new LinkResetEmailController();
-    app.post("/reset-email", [verifyJWT], (req, res, next) => {
+    app.post("/reset-email", [validate(createValidation), verifyJWT], (req, res, next) => {
         controller.create(req, res);
     });
 
-    app.get("/reset-email/confirm/:id", (req, res, next) => {
+    app.get("/reset-email/confirm/:id", validate(confirmValidation), (req, res, next) => {
         controller.confirm(req, res);
     });
 };
--- src/api/routes/LinkResetPassword.routes.js
+++ src/api/routes/LinkResetPassword.routes.js
@@ -1,13 +1,29 @@
 const LinkResetPasswordController = require("../controllers/LinkResetPasswordController");
 const { verifyJWT } = require("../middlewares/auth.middleware");
+const { validate, Joi } = require("express-validation");
 
 module.exports = (app) => {
+    const createValidation = {
+        body: Joi.object({
+            email: Joi.string().required(),
+        }),
+    };
+
+    const confirmValidation = {
+        body: Joi.object({
+            newPassword: Joi.string().required(),
+        }),
+        params: Joi.object({
+            id: Joi.string().required(),
+        }),
+    };
+
     const controller = new LinkResetPasswordController();
-    app.post("/reset-password", [verifyJWT], (req, res, next) => {
+    app.post("/reset-password", [validate(createValidation), verifyJWT], (req, res, next) => {
         controller.create(req, res);
     });
 
-    app.get("/reset-password/confirm/:id", (req, res, next) => {
+    app.get("/reset-password/confirm/:id", validate(confirmValidation), (req, res, next) => {
         controller.confirm(req, res);
     });
 };
